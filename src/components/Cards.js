2053e6f3fbed4feb0145cbda33c21a1173d212bc
Rhys <rhysconnor604@gmail.com>
2022-06-10 13:10:33 +0100

    Added NavBar Code, and edited some of the cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,43 +1,46 @@
-import { Card, CardGroup } from 'react-bootstrap';
+import { Card, CardGroup, ListGroup, ListGroupItem } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Cards = () => {
 
     return (
-        <CardGroup>
-  <Card border="dark" style={{ width: '18rem' }}>
-    <Card.Body>
-      <Card.Title>Me</Card.Title>
-      <Card.Text>
-        Days I'm in the office this week
-      </Card.Text>
-    </Card.Body>
-  </Card>
-  <Card border="dark" style={{ width: '18rem' }}>
-    <Card.Body>
-      <Card.Title>Favourites</Card.Title>
-      <Card.Text>
-        Who's in the office this week?
-      </Card.Text>
-    </Card.Body>
-  </Card>
-  <Card border="dark" style={{ width: '18rem' }}>
-    <Card.Body>
-      <Card.Title>Socials</Card.Title>
-      <Card.Text>
-        What events are happening this week?
-      </Card.Text>
-    </Card.Body>
-  </Card>
-  <Card border="dark" style={{ width: '18rem' }}>
-    <Card.Body>
-      <Card.Title>My Team</Card.Title>
-      <Card.Text>
-        Who from my team is in the office?
-      </Card.Text>
-    </Card.Body>
-  </Card>
-</CardGroup>
+      <cardgroup>
+      <Card style={{ width: '18rem' }}>
+  <Card.Body>
+    <Card.Title>My Favs</Card.Title>
+    <Card.Text>
+      My favs allows you to connect with your favorutites in the office, book a lunch/coffee date with them and organise a social event for after work.
+    </Card.Text>
+  </Card.Body>
+  <ListGroup className="list-group-flush">
+    <ListGroupItem>My Team</ListGroupItem>
+    <ListGroupItem>Socials</ListGroupItem>
+  </ListGroup>
+  <Card.Body>
+    <Card.Link href="#">Who's in the Office</Card.Link>
+    <Card.Link href="#">Book a Date </Card.Link>
+  </Card.Body>
+</Card>
+<br/>
+
+<Card style={{ width: '18rem' }}>
+<Card.Body>
+  <Card.Title>My Profile</Card.Title>
+  <Card.Text>
+    Information about yourself
+  </Card.Text>
+</Card.Body>
+<ListGroup className="list-group-flush">
+  <ListGroupItem> Edit your profile </ListGroupItem>
+  <ListGroupItem> My Favs </ListGroupItem>
+  <ListGroupItem> Your events </ListGroupItem>
+</ListGroup>
+<Card.Body>
+  <Card.Link href="#"> Calender </Card.Link>
+  <Card.Link href="#">  </Card.Link>
+</Card.Body>
+</Card>
+</cardgroup>
     )
   }
 
